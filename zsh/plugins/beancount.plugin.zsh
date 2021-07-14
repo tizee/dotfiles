fbd1ed68304d7a2096505c45ad07fa9e2f5bb75b
tizee <33030965+tizee@users.noreply.github.com>
2021-07-14 21:45:54 +0800

    feat(zsh): beancount

---
+++ zsh/plugins/beancount.plugin.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+alias bccheck="bean-check"
+# bean-report /path/to/my/file.beancount <report-name>
+# list all of available reports
+# bean-report --help-reports
+alias bcreport="bean-report"
+alias bcquery="bean-query"
+alias bcweb="bean-web"
