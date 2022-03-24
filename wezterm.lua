a5941f4e9536794d496a443766ee09339b02104f
tizee <33030965+tizee@users.noreply.github.com>
2022-03-24 16:22:46 +0800

    feat(wezterm): style

--- wezterm.lua
+++ wezterm.lua
@@ -6,41 +6,189 @@ local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 -- The filled in variant of the > symbol
 local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
 
+-- The powerline < symbol
+local LEFT_ARROW = utf8.char(0xe0b3);
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local edge_background = "#0b0022"
   local background = "#1b1032"
   local foreground = "#808080"
 
   if tab.is_active then
-    background = "#2b2042"
+    background = "#3c1361"
     foreground = "#c0c0c0"
   elseif hover then
     background = "#3b3052"
     foreground = "#909090"
   end
 
-  local edge_foreground = background
-
   -- ensure that the titles fit in the available space,
   -- and that we have room for the edges
-  local title = wezterm.truncate_right(tab.active_pane.title, max_width-2)
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width)
 
   return {
-    {Background={Color=edge_background}},
-    {Foreground={Color=edge_foreground}},
-    {Text=SOLID_LEFT_ARROW},
     {Background={Color=background}},
     {Foreground={Color=foreground}},
-    {Text=title},
-    {Background={Color=edge_background}},
-    {Foreground={Color=edge_foreground}},
-    {Text=SOLID_RIGHT_ARROW},
+    {Text=" "..tab.tab_index..":"..tab.active_pane.pane_id.." "..title.." "},
   }
 end)
 
+wezterm.on("update-right-status", function(window, pane)
+  -- Each element holds the text for a cell in a "powerline" style << fade
+  local cells = {};
+  local leader = ""
+  if window:leader_is_active() then
+    leader = "<LEADER>"
+    table.insert(cells,leader)
+  end
+  -- Figure out the cwd and host of the current pane.
+  -- This will pick up the hostname for the remote host if your
+  -- shell is using OSC 7 on the remote host.
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    cwd_uri = cwd_uri:sub(8);
+    local slash = cwd_uri:find("/")
+    local cwd = ""
+    local hostname = ""
+    if slash then
+      hostname = cwd_uri:sub(1, slash-1)
+      -- Remove the domain name portion of the hostname
+      local dot = hostname:find("[.]")
+      if dot then
+        hostname = hostname:sub(1, dot-1)
+      end
+      print(hostname)
+      -- and extract the cwd from the uri
+      cwd = cwd_uri:sub(slash)
+
+      table.insert(cells, cwd);
+      if string.len(hostname) > 0 then
+        table.insert(cells, hostname);
+      end
+    end
+  end
+
+  local date = wezterm.strftime("%a %b %-d %H:%M");
+  table.insert(cells, date);
+
+  -- An entry for each battery (typically 0 or 1 battery)
+  -- for _, b in ipairs(wezterm.battery_info()) do
+    -- table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+  -- end
+
+  -- Color palette for the backgrounds of each cell
+  local colors = {
+    "#3c1361",
+    "#52307c",
+    "#663a82",
+    "#7c5295",
+    "#b491c8",
+  };
+
+  -- Foreground color for the text across the fade
+  local text_fg = "#c0c0c0";
+
+  -- The elements to be formatted
+  local elements = {};
+  -- How many cells have been formatted
+  local num_cells = 0;
+
+  -- Translate a cell into elements
+  function push(text, is_last)
+    local cell_no = num_cells + 1
+    table.insert(elements, {Foreground={Color=text_fg}})
+    table.insert(elements, {Background={Color=colors[cell_no]}})
+    table.insert(elements, {Text=" "..text.." "})
+    if not is_last then
+      table.insert(elements, {Foreground={Color=colors[cell_no+1]}})
+      table.insert(elements, {Text=SOLID_LEFT_ARROW})
+    end
+    num_cells = num_cells + 1
+  end
+
+  while #cells > 0 do
+    local cell = table.remove(cells, 1)
+    push(cell, #cells == 0)
+  end
+
+  window:set_right_status(wezterm.format(elements));
+end);
+
 return {
   -- general settings <-
-  colors = { compose_cursor = "green" },
+  colors = {
+      compose_cursor = "green",
+      tab_bar = {
+      -- The color of the strip that goes along the top of the window
+      -- (does not apply when fancy tab bar is in use)
+      background = "#0b0022",
+
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+        -- The color of the background area for the tab
+        bg_color = "#2b2042",
+        -- The color of the text for the tab
+        fg_color = "#c0c0c0",
+
+        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+        -- label shown for this tab.
+        -- The default is "Normal"
+        intensity = "Normal",
+
+        -- Specify whether you want "None", "Single" or "Double" underline for
+        -- label shown for this tab.
+        -- The default is "None"
+        underline = "None",
+
+        -- Specify whether you want the text to be italic (true) or not (false)
+        -- for this tab.  The default is false.
+        italic = false,
+
+        -- Specify whether you want the text to be rendered with strikethrough (true)
+        -- or not for this tab.  The default is false.
+        strikethrough = false,
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = "#1b1032",
+        fg_color = "#808080",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = "#3b3052",
+        fg_color = "#909090",
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab_hover`.
+      },
+
+      -- The new tab button that let you create new tabs
+      new_tab = {
+        bg_color = "#1b1032",
+        fg_color = "#808080",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over the new tab button
+      new_tab_hover = {
+        bg_color = "#3b3052",
+        fg_color = "#909090",
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab_hover`.
+      }
+    }
+  },
   check_for_updates = true,
   exit_behavior="CloseOnCleanExit", -- only exit with a successful status
   default_cursor_style = "SteadyUnderline",
