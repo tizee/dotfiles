0740cbfc4a3111eb9ebca22068c15f824f60915e
tizee <33030965+tizee@users.noreply.github.com>
2022-04-05 18:47:49 +0800

    feat(zsh): fzf widget

--- zsh/widgets/fzf.widget.zsh
+++ zsh/widgets/fzf.widget.zsh
@@ -3,9 +3,9 @@
 # use fzf to kill proccesses
 fzfkill_widget() {
    (command -v fzf::kill_pid) && fzf::kill_pid
-   # zle reset-prompt
+   zle reset-prompt
 }
 
-# zle -N fzf-redraw-prompt
+zle -N fzf-redraw-prompt
 zle -N fzfkill_widget
 bindkey '^k'  fzfkill_widget #ctrl+k
