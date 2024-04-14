d547110576a41d2881f830183b7fdfe9e30d00e9
tizee <33030965+tizee@users.noreply.github.com>
2024-04-15 00:13:24 +0800

    refactor(ipython): update script and add ipython_config.py

--- python/ipython/00-startup.py
+++ python/ipython/00-startup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
-import json
-import time
-import datetime as dt
-import collections
-import sys
+
+# see ipython_config.py
+
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    import requests
+except ImportError as e :
+    pass
 
 # pretty printing
 # duplicate with pprint in rich.pretty
@@ -16,39 +20,33 @@ try:
     from rich.pretty import pprint
     from rich.text import Text
     from rich.table import Table
-    # pretty.install()
+    pretty.install()
     from IPython import get_ipython
     ipython = get_ipython()
-    ipython.magic("load_ext rich")
+    # ipython auto-reload
+    ipython.run_line_magic("load_ext", "autoreload")
+    ipython.run_line_magic("autoreload", "2")
+
     link_github = Text("github","link https://github.com")
 except ImportError as e :
     pass
 
 
-def _repr_dict(d):
+
+def _print_dict(d):
     """https://stackoverflow.com/questions/25118698/print-python-dictionary-with-utf8-values"""
-    print('{%s}' % ',\n'.join("'%s': '%s'" % pair for pair in d.iteritems()))
+    pprint('{%s}' % ',\n'.join("'%s': '%s'" % pair for pair in d.iteritems()))
 
 
-def _json_dumps(dict_data, indent=4):
+def _print_json(dict_data, indent=4):
     """handle non-latin characters"""
-    print(json.dumps(dict_data, indent=indent, ensure_ascii=False))
+    pprint(json.dumps(dict_data, indent=indent, ensure_ascii=False))
 
 
-repr_dict = _repr_dict
 pp = pprint
-json_dumps = _json_dumps
+print_dict = _print_dict
+print_json = _print_json
 
-print("(imported datetime, os, pprint, re, sys, time, json)")
-print("(00-startup.py)")
-
-try:
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
-    import requests
-except ImportError as e :
-    pass
 
 def is_reload():
     """ export LENS_ADMIN_DEBUG=1 """
@@ -58,10 +56,4 @@ def is_reload():
     return False
 
 # http://shawnleezx.github.io/blog/2015/08/03/some-notes-on-ipython-startup-script/
-# _is_reload = is_reload()
-# print("export RELOAD={}".format(_is_reload))
-# if _is_reload:
-#     # ipython auto-reload
-
-#     ipython.magic("load_ext autoreload")
-#     ipython.magic("autoreload 2")
+pp("(00-startup.py)")
